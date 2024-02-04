f150114137b14055b6d327356197a448e009620d
JoelWekesa <joelwekesa.jw@gmail.com>
2024-02-04 06:20:18 +0300

    Switch to client

--- src/components/counter.tsx
+++ src/components/counter.tsx
@@ -1,12 +1,28 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+
 const Counter = () => {
-	const now = new Date().setHours(0, 0, 0, 0);
-	const yearEnd = new Date('12/31/2023').setHours(0, 0, 0, 0);
+	const [count, setCount] = useState(0);
+	const [loading, setLoading] = useState(true);
+	useEffect(() => {
+		const handleNew = () => {
+			const now = new Date().setHours(0, 0, 0, 0);
+			const yearEnd = new Date('12/31/2023').setHours(0, 0, 0, 0);
 
-	const diff = now - yearEnd;
+			const diff = now - yearEnd;
 
-	const count = (diff / (24 * 1000 * 60 * 60)) * 10;
+			const count = (diff / (24 * 1000 * 60 * 60)) * 10;
+			setCount(count);
+			setLoading(false);
+		};
 
-	return <p className='text-center text-white text-9xl'>{count}</p>;
+		handleNew();
+	}, []);
+
+	return (
+		<p className='text-center text-white text-9xl'>{loading ? '...' : count}</p>
+	);
 };
 
 export default Counter;
